d8473d67b0f996d30154d9e0d53189ba96ed3bb3
MalikaGzn <malikagozikhonova@gmail.com>
2023-11-11 23:43:22 +0500

    Initial Commit

---
+++ Yummy Co/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yummy Co
+//
+//  Created by Malika 💕 on 11/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
